128a0c0d7ffdaf23f7314b460334d105ab4173bd
Santhoshkumar <santhoshchellamuthuc@gmail.com>
2024-10-02 23:14:10 +0530

    Add project files.

---
+++ DataAccessLayer/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DataAccessLayer
+{
+    // Models/Employee.cs
+    public class Employee
+    {
+        public long ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Department { get; set; }
+
+        // Navigation property
+        public virtual Attendance Attendance { get; set; }
+    }
+
+    // Models/Attendance.cs
+    public class Attendance
+    {
+        public long AttendanceID { get; set; }
+        public long EmployeeID { get; set; }
+        public DateTime Date { get; set; }
+        public TimeSpan CheckInTime { get; set; }
+        public TimeSpan CheckOutTime { get; set; }
+
+        // Navigation property
+        public virtual Employee Employee { get; set; }
+    }
+
+}
---
+++ DataAccessLayer/EAMDbcontext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class EAMDbcontext:DbContext
+    {
+        public EAMDbcontext(DbContextOptions<EAMDbcontext>options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>()
+                .HasOne(e => e.Attendance)
+                .WithOne(a => a.Employee)
+                .HasForeignKey<Attendance>(a => a.EmployeeID);
+        }
+    }
+}
